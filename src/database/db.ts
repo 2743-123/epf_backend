cae10d73a8a410a211452fd6962ba1c16d17f602
asif <asifandhi01@gmail.com>
2025-11-08 15:19:57 +0530

    err

--- src/database/db.ts
+++ src/database/db.ts
@@ -6,10 +6,8 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  schema: "public",
   url: process.env.DATABASE_URL, // 👈 direct render url
   synchronize: true,
-  logging: false,
   ssl: {
     rejectUnauthorized: false, // 👈 important for Render connection
   },
